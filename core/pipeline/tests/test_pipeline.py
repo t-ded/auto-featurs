3e99fdfe3c6f6734cdc01d4ad80f791591149d0c
Tomáš Děd <tomas-ded@seznam.cz>
2025-11-15 09:55:31 +0100

    test: test layering of pipeline

--- core/pipeline/tests/test_pipeline.py
+++ core/pipeline/tests/test_pipeline.py
@@ -22,6 +22,24 @@ class TestPipeline:
             pl.DataFrame({'NUMERIC_FEATURE': [0, 1, 2, 3, 4, 5], 'NUMERIC_FEATURE_pow_2': [0, 1, 4, 9, 16, 25]}),
         )
 
+    def test_basic_layering(self) -> None:
+        pipeline = Pipeline(column_types={'NUMERIC_FEATURE': ColumnType.NUMERIC})
+        pipeline = pipeline.with_polynomial(subset=cs.numeric(), degrees=[2])
+        pipeline = pipeline.with_layer()
+        pipeline = pipeline.with_polynomial(subset=cs.numeric(), degrees=[2])
+        df = pl.LazyFrame({'NUMERIC_FEATURE': [0, 1, 2, 3, 4, 5]})
+
+        res = pipeline.collect(df)
+
+        assert_frame_equal(
+            res,
+            pl.DataFrame({
+                'NUMERIC_FEATURE': [0, 1, 2, 3, 4, 5],
+                'NUMERIC_FEATURE_pow_2': [0, 1, 4, 9, 16, 25],
+                'NUMERIC_FEATURE_pow_2_pow_2': [0, 1, 16, 81, 256, 625],
+            })
+        )
+
     def test_basic_sample_with_all_transformers(self) -> None:
         pipeline = Pipeline(column_types={'NUMERIC_FEATURE': ColumnType.NUMERIC, 'NUMERIC_FEATURE_2': ColumnType.NUMERIC})
         pipeline = pipeline.with_polynomial(subset=cs.numeric(), degrees=[2, 3])
