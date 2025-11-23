784396d4a1bd5bc0264c1316f2e0173e60b32407
Tomáš Děd <tomas-ded@seznam.cz>
2025-11-23 10:48:04 +0100

    feat: add lagged transformer

---
+++ core/transformers/lagged_transformer.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+import polars as pl
+
+from core.base.column_specification import ColumnType
+from core.transformers.base import Transformer
+
+
+class LaggedTransformer(Transformer):
+    def __init__(self, column: str, column_type: ColumnType, lag: int, fill_value: Any = None) -> None:
+        self._column = column
+        self._column_type = column_type
+        self._lag = lag
+        self._fill_value = fill_value
+
+    def input_type(self) -> set[ColumnType] | tuple[set[ColumnType], ...]:
+        return ColumnType.ANY()
+
+    @classmethod
+    def is_commutative(cls) -> bool:
+        return True
+
+    def _return_type(self) -> ColumnType:
+        return self._column_type
+
+    def _transform(self) -> pl.Expr:
+        return pl.col(self._column).shift(self._lag, fill_value=self._fill_value)
+
+    def _name(self, transform: pl.Expr) -> pl.Expr:
+        return transform.alias(f'{self._column}_lagged_{self._lag}')
