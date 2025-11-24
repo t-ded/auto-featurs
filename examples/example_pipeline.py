383a5b9e2b015cf5fd3accce4fe7c979c0da97cb
Tomáš Děd <tomas-ded@seznam.cz>
2025-11-24 20:45:28 +0100

    doc: add super simple example notebook

--- examples/example_pipeline.py
+++ examples/example_pipeline.py
@@ -1,29 +1,24 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "auto-featurs",
+#     "polars==1.35.2",
+# ]
+#
+# [tool.uv.sources]
+# auto-featurs = { path = "../", editable = true }
+# ///
+
 import marimo
 
-__generated_with = "0.17.7"
+__generated_with = "0.18.0"
 app = marimo.App(width="medium")
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import polars as pl
-
-    from datetime import UTC
-    from datetime import datetime
-    return UTC, datetime, mo, pl
-
-
-@app.cell
-def _():
-    import auto_featurs
-    return
 
 
 @app.cell
 def _(mo):
     mo.md(rf"""
-    ## Data Setup
+    # Initial Look at the Data
     """)
     return
 
@@ -36,37 +31,157 @@ def _(BASIC_FRAME):
 
 @app.cell
 def _(mo):
-    mo.md(r"""
-    ## Pipeline Setup
+    mo.md(rf"""
+    # Pipeline Setup
     """)
     return
 
 
 @app.cell
-def _(Pipeline):
-    pipeline = Pipeline
+def _(mo):
+    mo.md(r"""
+    ## Unoptimized
+    """)
+    return
+
+
+@app.cell
+def _(
+    ArithmeticAggregations,
+    BASIC_FRAME,
+    ColumnSpecification,
+    ColumnType,
+    Comparisons,
+    Pipeline,
+):
+    pipeline = Pipeline(
+        schema=[
+            ColumnSpecification.numeric(name='NUMERIC_FEATURE'),
+            ColumnSpecification.ordinal(name='CATEGORICAL_FEATURE'),
+            ColumnSpecification.datetime(name='DATE_FEATURE'),
+        ],
+    )
+
+    pipeline = (
+        pipeline
+        .with_polynomial(subset='NUMERIC_FEATURE', degrees=[2])
+        .with_arithmetic_aggregation(
+            subset='NUMERIC_FEATURE',
+            aggregations=[ArithmeticAggregations.COUNT],
+            time_windows=['2d'],
+            index_column_name='DATE_FEATURE',
+            # over_columns_combinations=[[], ['GROUPING_FEATURE_NUM']],  # TODO: Uncomment this with new polars release (allowing window expressions in aggregation)
+        )
+        .with_new_layer()
+        .with_comparison(
+            left_subset=ColumnType.NUMERIC,
+            right_subset=ColumnType.NUMERIC,
+            comparisons=[Comparisons.EQUAL],
+        )
+    )
+
+    res = pipeline.collect(df=BASIC_FRAME)
+    res
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Optimized
+    """)
+    return
+
+
+@app.cell
+def _(
+    ArithmeticAggregations,
+    BASIC_FRAME,
+    ColumnSpecification,
+    ColumnType,
+    Comparisons,
+    OptimizationLevel,
+    Pipeline,
+):
+    pipeline_optimized = Pipeline(
+        schema=[
+            ColumnSpecification.numeric(name='NUMERIC_FEATURE'),
+            ColumnSpecification.ordinal(name='CATEGORICAL_FEATURE'),
+            ColumnSpecification.datetime(name='DATE_FEATURE'),
+        ],
+        optimization_level=OptimizationLevel.DEDUPLICATE_COMMUTATIVE,
+    )
+
+    pipeline_optimized = (
+        pipeline_optimized
+        .with_polynomial(subset='NUMERIC_FEATURE', degrees=[2])
+        .with_arithmetic_aggregation(
+            subset='NUMERIC_FEATURE',
+            aggregations=[ArithmeticAggregations.COUNT],
+            time_windows=['2d'],
+            index_column_name='DATE_FEATURE',
+            # over_columns_combinations=[[], ['GROUPING_FEATURE_NUM']],  # TODO: Uncomment this with new polars release (allowing window expressions in aggregation)
+        )
+        .with_new_layer()
+        .with_comparison(
+            left_subset=ColumnType.NUMERIC,
+            right_subset=ColumnType.NUMERIC,
+            comparisons=[Comparisons.EQUAL],
+        )
+    )
+
+    res_optimized = pipeline_optimized.collect(df=BASIC_FRAME)
+    res_optimized
     return
 
 
 @app.cell
 def _(mo):
     mo.md(rf"""
-    ## Apendix
+    # Apendix
     """)
     return
 
 
 @app.cell
-def _(UTC, datetime, pl):
+def _():
+    import marimo as mo
+    import polars as pl
+
+    from datetime import date
+    from datetime import timedelta
+    from datetime import UTC
+    return date, mo, pl
+
+
+@app.cell
+def _():
+    from auto_featurs.core.base.column_specification import ColumnSpecification
+    from auto_featurs.core.base.column_specification import ColumnType
+
+    from auto_featurs.core.pipeline.optimizer import OptimizationLevel
+    from auto_featurs.core.pipeline.pipeline import Pipeline
+
+    from auto_featurs.core.transformers.aggregating_transformers import ArithmeticAggregations
+    from auto_featurs.core.transformers.comparison_transformers import Comparisons
+    return (
+        ArithmeticAggregations,
+        ColumnSpecification,
+        ColumnType,
+        Comparisons,
+        OptimizationLevel,
+        Pipeline,
+    )
+
+
+@app.cell
+def _(date, pl):
     BASIC_FRAME = pl.LazyFrame(
         {
             'NUMERIC_FEATURE': [0, 1, 2, 3, 4, 5],
-            'NUMERIC_FEATURE_2': [0, -1, -2, -3, -4, -5],
             'CATEGORICAL_FEATURE': ['A', 'B', 'C', 'D', 'E', 'F'],
-            'CATEGORICAL_FEATURE_2': ['F', 'E', 'D', 'C', 'B', 'A'],
             'GROUPING_FEATURE_NUM': ['ZERO', 'ODD', 'EVEN', 'ODD', 'EVEN', 'ODD'],
-            'GROUPING_FEATURE_CAT_2': ['CONSONANT', 'VOWEL', 'CONSONANT', 'CONSONANT', 'CONSONANT', 'VOWEL'],
-            'DATE_FEATURE': [datetime(year=2_000, month=1, day=i, tzinfo=UTC) for i in range(1, 7)],
+            'DATE_FEATURE': [date(year=2_000, month=1, day=i) for i in range(1, 7)],
         },
     )
     return (BASIC_FRAME,)
