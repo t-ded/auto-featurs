5cbcd5a1b54e7ad47560c9377f9c8fa7d5d28b27
Tomáš Děd <tomas-ded@seznam.cz>
2025-12-05 20:35:58 +0100

    feat: add hour of day and month of year transformers and test all

--- src/auto_featurs/transformers/datetime_transformers.py
+++ src/auto_featurs/transformers/datetime_transformers.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from enum import Enum
 
 import polars as pl
@@ -7,7 +8,7 @@ from auto_featurs.base.column_specification import ColumnType
 from auto_featurs.transformers.base import Transformer
 
 
-class DayOfWeekTransformer(Transformer):
+class SeasonalTransformer(Transformer, ABC):
     def __init__(self, column: str | ColumnSpecification) -> None:
         self._column = column if isinstance(column, str) else column.name
 
@@ -21,6 +22,16 @@ class DayOfWeekTransformer(Transformer):
     def _return_type(self) -> ColumnType:
         return ColumnType.ORDINAL
 
+
+class HourOfDayTransformer(SeasonalTransformer):
+    def _transform(self) -> pl.Expr:
+        return pl.col(self._column).dt.hour()
+
+    def _name(self, transform: pl.Expr) -> pl.Expr:
+        return transform.alias(f'{self._column}_hour_of_day')
+
+
+class DayOfWeekTransformer(SeasonalTransformer):
     def _transform(self) -> pl.Expr:
         return pl.col(self._column).dt.weekday()
 
@@ -28,5 +39,15 @@ class DayOfWeekTransformer(Transformer):
         return transform.alias(f'{self._column}_day_of_week')
 
 
+class MonthOfYearTransformer(SeasonalTransformer):
+    def _transform(self) -> pl.Expr:
+        return pl.col(self._column).dt.month()
+
+    def _name(self, transform: pl.Expr) -> pl.Expr:
+        return transform.alias(f'{self._column}_month_of_year')
+
+
 class SeasonalOperation(Enum):
+    HOUR_OF_DAY = HourOfDayTransformer
     DAY_OF_WEEK = DayOfWeekTransformer
+    MONTH_OF_YEAR = MonthOfYearTransformer
--- src/auto_featurs/transformers/tests/test_datetime_transformers.py
+++ src/auto_featurs/transformers/tests/test_datetime_transformers.py
@@ -1,21 +1,52 @@
+from datetime import UTC
+from datetime import datetime
+from typing import Optional
+
+import polars as pl
 
 from auto_featurs.base.column_specification import ColumnSpecification
 from auto_featurs.transformers.datetime_transformers import DayOfWeekTransformer
-from auto_featurs.utils.utils_for_tests import BASIC_FRAME
-from auto_featurs.utils.utils_for_tests import assert_new_columns_in_frame
+from auto_featurs.transformers.datetime_transformers import HourOfDayTransformer
+from auto_featurs.transformers.datetime_transformers import MonthOfYearTransformer
 
 
-class TestDayOfWeekTransformer:
+class TestSeasonalTransformers:
     def setup_method(self) -> None:
+        self._hour_of_day_transformer = HourOfDayTransformer(column=ColumnSpecification.datetime(name='DATE_FEATURE'))
         self._day_of_week_transformer = DayOfWeekTransformer(column=ColumnSpecification.datetime(name='DATE_FEATURE'))
+        self._month_of_year_transformer = MonthOfYearTransformer(column=ColumnSpecification.datetime(name='DATE_FEATURE'))
 
     def test_name_and_output_type(self) -> None:
+        assert self._hour_of_day_transformer.output_column_specification == ColumnSpecification.ordinal(name='DATE_FEATURE_hour_of_day')
         assert self._day_of_week_transformer.output_column_specification == ColumnSpecification.ordinal(name='DATE_FEATURE_day_of_week')
+        assert self._month_of_year_transformer.output_column_specification == ColumnSpecification.ordinal(name='DATE_FEATURE_month_of_year')
+
+    @staticmethod
+    def _get_df(months: Optional[list[int]] = None, days: Optional[list[int]] = None, hours: Optional[list[int]] = None) -> pl.DataFrame:
+        months = months or [1, 1, 1]
+        days = days or [1, 1, 1]
+        hours = hours or [1, 1, 1]
+
+        datetimes: list[datetime] = []
+        for month, day, hour in zip(months, days, hours, strict=True):
+            datetimes.append(datetime(year=2018, month=month, day=day, hour=hour, tzinfo=UTC))
+
+        return pl.DataFrame({'DATE_FEATURE': datetimes})
+
+    def test_hour_of_day_transform(self) -> None:
+        hours = [20, 21, 22]
+        df = self._get_df(hours=hours)
+        df = df.with_columns(self._hour_of_day_transformer.transform())
+        assert df['DATE_FEATURE_hour_of_day'].to_list() == hours
 
     def test_day_of_week_transform(self) -> None:
-        df = BASIC_FRAME.with_columns(self._day_of_week_transformer.transform())
-        assert_new_columns_in_frame(
-            original_frame=BASIC_FRAME,
-            new_frame=df,
-            expected_new_columns={'DATE_FEATURE_day_of_week': [6, 7, 1, 2, 3, 4]},
-        )
+        days = [1, 2, 3]
+        df = self._get_df(days=days)
+        df = df.with_columns(self._day_of_week_transformer.transform())
+        assert df['DATE_FEATURE_day_of_week'].to_list() == days
+
+    def test_month_of_year_transform(self) -> None:
+        months = [10, 11, 12]
+        df = self._get_df(months=months)
+        df = df.with_columns(self._month_of_year_transformer.transform())
+        assert df['DATE_FEATURE_month_of_year'].to_list() == months
