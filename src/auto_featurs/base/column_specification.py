c8fa49279f4b2d6651a1fcba651e65335a42a2f7
Tomáš Děd <tomas-ded@seznam.cz>
2025-12-11 09:46:19 +0100

    feat: implement ColumnSelector and enable and op on ColumnType and ColumnRole

--- src/auto_featurs/base/column_specification.py
+++ src/auto_featurs/base/column_specification.py
@@ -4,6 +4,7 @@ from collections.abc import Sequence
 from dataclasses import dataclass
 from enum import Enum
 from enum import auto
+from typing import Optional
 
 type ColumnSelection = str | Sequence[str] | ColumnType | Sequence[ColumnType]
 type ColumnSet = list[ColumnSpecification]
@@ -21,12 +22,38 @@ class ColumnType(Enum):
     def ANY(cls) -> set[ColumnType]:  # noqa: N802
         return set(cls)
 
+    def __and__(self, other: object) -> ColumnSelector:
+        if isinstance(other, ColumnRole):
+            return ColumnSelector(type_selector=ColumnTypeSelector({self}), role_selector=ColumnRoleSelector({other}))
+        elif isinstance(other, ColumnRoleSelector):
+            return ColumnSelector(type_selector=ColumnTypeSelector({self}), role_selector=other)
+        else:
+            raise TypeError(f'Cannot add {type(other)} to ColumnType')
+
+    def __invert__(self) -> ColumnTypeSelector:
+        return ColumnTypeSelector(types=ColumnType.ANY() - {self})
+
 
 class ColumnRole(Enum):
     LABEL = auto()
     IDENTIFIER = auto()
     TIME_INFO = auto()
     FEATURE = auto()
+
+    @classmethod
+    def ANY(cls) -> set[ColumnRole]:  # noqa: N802
+        return set(cls)
+
+    def __and__(self, other: object) -> ColumnSelector:
+        if isinstance(other, ColumnType):
+            return ColumnSelector(type_selector=ColumnTypeSelector({other}), role_selector=ColumnRoleSelector({self}))
+        elif isinstance(other, ColumnTypeSelector):
+            return ColumnSelector(type_selector=other, role_selector=ColumnRoleSelector({self}))
+        else:
+            raise TypeError(f'Cannot add {type(other)} to ColumnRole')
+
+    def __invert__(self) -> ColumnRoleSelector:
+        return ColumnRoleSelector(roles=ColumnRole.ANY() - {self})
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -58,3 +85,60 @@ class ColumnSpecification:
     @classmethod
     def datetime(cls, name: str, role: ColumnRole = ColumnRole.FEATURE) -> ColumnSpecification:
         return ColumnSpecification(name=name, column_type=ColumnType.DATETIME, column_role=role)
+
+
+class ColumnTypeSelector:
+    def __init__(self, types: Optional[set[ColumnType]] = None) -> None:
+        self._types = types if types is not None else ColumnType.ANY()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ColumnTypeSelector):
+            raise TypeError(f'Cannot compare {type(other)} to ColumnTypeSelector')
+        return self._types == other.types
+
+    @property
+    def types(self) -> set[ColumnType]:
+        return self._types
+
+
+class ColumnRoleSelector:
+    def __init__(self, roles: Optional[set[ColumnRole]] = None) -> None:
+        self._roles = roles if roles is not None else ColumnRole.ANY()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ColumnRoleSelector):
+            raise TypeError(f'Cannot compare {type(other)} to ColumnRoleSelector')
+        return self._roles == other.roles
+
+    @property
+    def roles(self) -> set[ColumnRole]:
+        return self._roles
+
+
+class ColumnSelector:
+    def __init__(
+        self,
+        names: Optional[set[str]] = None,
+        type_selector: ColumnTypeSelector = ColumnTypeSelector(),
+        role_selector: ColumnRoleSelector = ColumnRoleSelector(),
+    ) -> None:
+        self._names = names
+        self._type_selector = type_selector
+        self._role_selector = role_selector
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ColumnSelector):
+            raise TypeError(f'Cannot compare {type(other)} to ColumnSelector')
+        return self._names == other.names
+
+    @property
+    def names(self) -> Optional[set[str]]:
+        return self._names
+
+    @property
+    def types(self) -> set[ColumnType]:
+        return self._type_selector.types
+
+    @property
+    def roles(self) -> set[ColumnRole]:
+        return self._role_selector.roles
--- src/auto_featurs/base/tests/test_column_specification.py
+++ src/auto_featurs/base/tests/test_column_specification.py
@@ -1,18 +1,58 @@
 from auto_featurs.base.column_specification import ColumnRole
+from auto_featurs.base.column_specification import ColumnRoleSelector
+from auto_featurs.base.column_specification import ColumnSelector
 from auto_featurs.base.column_specification import ColumnSpecification
 from auto_featurs.base.column_specification import ColumnType
-
-
-def test_any_column_type() -> None:
-    assert ColumnType.ANY() == {
-        ColumnType.NUMERIC,
-        ColumnType.BOOLEAN,
-        ColumnType.NOMINAL,
-        ColumnType.ORDINAL,
-        ColumnType.TEXT,
-        ColumnType.DATETIME,
-    }
+from auto_featurs.base.column_specification import ColumnTypeSelector
 
 
 def test_spec_creation() -> None:
     assert ColumnSpecification.numeric(name='a', role=ColumnRole.LABEL) == ColumnSpecification(name='a', column_type=ColumnType.NUMERIC, column_role=ColumnRole.LABEL)
+
+
+class TestColumnType:
+    def test_any_column_type(self) -> None:
+        assert ColumnType.ANY() == {
+            ColumnType.NUMERIC,
+            ColumnType.BOOLEAN,
+            ColumnType.NOMINAL,
+            ColumnType.ORDINAL,
+            ColumnType.TEXT,
+            ColumnType.DATETIME,
+        }
+
+    def test_and(self) -> None:
+        assert ColumnType.NUMERIC & ColumnRole.LABEL == ColumnSelector(
+            type_selector=ColumnTypeSelector({ColumnType.NUMERIC}),
+            role_selector=ColumnRoleSelector({ColumnRole.LABEL}),
+        )
+        assert ColumnType.NUMERIC & ColumnRoleSelector({ColumnRole.LABEL}) == ColumnSelector(
+            type_selector=ColumnTypeSelector({ColumnType.NUMERIC}),
+            role_selector=ColumnRoleSelector({ColumnRole.LABEL}),
+        )
+
+    def test_invert(self) -> None:
+        assert ~ColumnType.NUMERIC == ColumnTypeSelector(types=ColumnType.ANY() - {ColumnType.NUMERIC})
+
+
+class TestColumnRole:
+    def test_any_column_type(self) -> None:
+        assert ColumnRole.ANY() == {
+            ColumnRole.LABEL,
+            ColumnRole.IDENTIFIER,
+            ColumnRole.TIME_INFO,
+            ColumnRole.FEATURE,
+        }
+
+    def test_and(self) -> None:
+        assert ColumnRole.LABEL & ColumnType.NUMERIC == ColumnSelector(
+            type_selector=ColumnTypeSelector({ColumnType.NUMERIC}),
+            role_selector=ColumnRoleSelector({ColumnRole.LABEL}),
+        )
+        assert ColumnRole.LABEL & ColumnTypeSelector({ColumnType.NUMERIC}) == ColumnSelector(
+            type_selector=ColumnTypeSelector({ColumnType.NUMERIC}),
+            role_selector=ColumnRoleSelector({ColumnRole.LABEL}),
+        )
+
+    def test_invert(self) -> None:
+        assert ~ColumnRole.LABEL == ColumnRoleSelector(roles=ColumnRole.ANY() - {ColumnRole.LABEL})
